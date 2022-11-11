0f9982bd0b86ee129ce10aeaf025a24ce9a3e63b
adik999 <kumar.aditya9999@gmail.com>
2022-11-12 01:01:47 +0530

    2nd_or_3rd update

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import pickle
 from sklearn.linear_model import LogisticRegression
 from flask import Flask, request, jsonify, render_template
 app = Flask(__name__)
-model = pickle.load(open("model.pkl","rb"))
+# model = pickle.load(open("model.pkl","rb"))
+import sup as m
 @app.route("/")
 def Home():
     return render_template("index.html")
@@ -12,7 +13,7 @@ def Home():
 def predict():
     float_features = [float(x) for x in request.form.values()]
     features = [np.array(float_features)]
-    prediction = model.predict(features)
+    prediction = m.function_made(features)
     ans =  render_template("index.html", prediction_text = prediction)
     temp_dict = {1:'Pass The Test',0:'Fail The Test'}
     prediction = temp_dict[int(prediction[0])]
--- sup.py
+++ sup.py
@@ -7,17 +7,20 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from joblib import dump, load
 import pickle
-df = pd.read_csv("hearing_test.csv")
-X = df.drop('test_result', axis=1)
-y = df['test_result']
-print(df.head())
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=101)
-scaler = StandardScaler()
-scaler.fit(X_train)
-scaled_X_train = scaler.transform(X_train)
-scaled_X_test = scaler.transform(X_test)
-model = LogisticRegression()
-model.fit(scaled_X_train,y_train)
-pickle.dump(model,open("model.pkl","wb"))
-
+def function_made(vals):
+    df = pd.read_csv("hearing_test.csv")
+    X = df.drop('test_result', axis=1)
+    y = df['test_result']
+    print(df.head())
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=101)
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+    scaled_X_train = scaler.transform(X_train)
+    scaled_X_test = scaler.transform(X_test)
+    model = LogisticRegression()
+    model.fit(scaled_X_train,y_train)
+    pickle.dump(model,open("model.pkl","wb"))
+    X_test = np.array(vals)
+    return model.predict(X_test)
 # print(help(train_test_split()))
+# function_made(vals)
